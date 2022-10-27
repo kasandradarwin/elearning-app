58896b843161e05f5da897baa1bf13f244727086
Kasandra Darwin <kasandradarwin@gmail.com>
2022-10-27 04:04:43 +0000

    added landing pages page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/landing_pages'
+  get 'static_pages/privacy_policy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
